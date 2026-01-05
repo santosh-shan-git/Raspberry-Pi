7f718cb71f93a9464702ee402c5893da68f3840a
Santosh Shanmugam <135924533+santosh-shan-git@users.noreply.github.com>
2026-01-05 13:20:52 -0500

    Update servo_motor.py

---
+++ servo_motor.py
@@ -0,0 +1,28 @@
+# # import RPi.GPIO as GPIO
+# # import time
+# # GPIO.setmode(GPIO.BOARD)
+# # green=8
+# # vcc=4
+# # gnd=6
+# # GPIO.setup(green, GPIO.OUT)
+# # p=GPIO.PWM(green, 50)
+# # p.start(12.5)
+# # time.sleep(1)
+# # p.ChangeDutyCycle(7.5)
+# # time.sleep(1)
+
+
+# import RPi.GPIO as GPIO
+# import time
+# import random
+# GPIO.setmode(GPIO.BOARD)
+# gp=26
+
+# GPIO.setup(gp,GPIO.OUT)
+# p=GPIO.PWM(gp, 50)
+# p.start(12.5)
+# time.sleep(1)
+# p.ChangeDutyCycle(7.5)
+# time.sleep(1)
+# p.ChangeDutyCycle(2.5)
+# time.sleep(1)
